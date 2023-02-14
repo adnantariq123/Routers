ae663c162085620e0c47277b7d7ab3ffa7741fd7
adnantariq123 <ahmedadnan123@hotmail.com>
2023-02-13 20:05:21 -0500

    function returns name property IF it exist
    function takes array of objects, and check if any of those objects name a property with "NAME"

--- react-router/src/Components/SomeTrinksQuestions.jsx
+++ react-router/src/Components/SomeTrinksQuestions.jsx
@@ -44,38 +44,53 @@ export const SomeTrinksQuestions = ()=>{
         }
     }
 
-    StringFinder("Mary had a litte Dragon", "Dragon")
+    const GetNames=(array)=>{
+        let result=[];
+
+        for (let i=0; i<array.length;i++) {
+            if (array[i].hasOwnProperty("name")===true) {
+                result.push(array[i].name)
+            }
+        }
+        return result;
+    }
+
+    console.log(GetNames([{a:1}, {a:2,name:"parveen"}, {name:"oriana"}]))
     
 
     return (
-        <>
+        <div className="Tricky">
 
         <h4>Write a function that takes <u>ANY number of arguments</u> and returns that <u>sum</u> of all those arguemnts</h4>
         <p>use the spread operator in the function </p>
         <p>sumOFALL=(<b>...argumentsALL</b>)</p>
-        <p>{sumOFALL(1,2,3)}</p>
-        <p>{sumOFALL(1,2,3,4,5)}</p>
-        <p>{sumOFALL(1,2,3,4,5,6,7,8,9)}</p>
-        <p>{sumOFALL(100,200,300)}</p>
+        <p>sumOFALL(1,2,3) - {sumOFALL(1,2,3)}</p>
+        <p>sumOFALL(1,2,3,4,5) - {sumOFALL(1,2,3,4,5)}</p>
+        <p>sumOFALL(1,2,3,4,5,6,7,8,9) - {sumOFALL(1,2,3,4,5,6,7,8,9)}</p>
+        <p>sumOFALL(100,200,300) - {sumOFALL(100,200,300)}</p>
 
         <hr/>
         <h4>Write a fucntion that take two string, A and B and returns a Right! or a Wrong! weather B is inside A in a 'case-INsensitive way' </h4>
         <p>StringFinder("Mary had a litte Lamb", "Dragon")</p>
         <p>{StringFinder("Mary had a litte Lamb", "Dragon")}</p>
 
-        <br/>
+
         <p>StringFinder("Mary had a litte LAMB", "lamb")</p>
         <p>{StringFinder("Mary had a litte LAMB", "lamb")}</p>
+        <hr/>
+
+
+        <h4>Write a function that takes an array of objects, and returns an array of object "name" property , ONLY if that property exists</h4>
+        <p>GetNames(an Array of Object) -  look in Console.log()</p>
+
         <h4/>
-
-
         <h4> Some Tricky Javascript Questions</h4>
         <p> take a look at the console.log</p>
 
 
 
 
-        </>
+        </div>
 
         
     )
